945a64d44abd15a2e1d9f5efe4a13610eb17d551
garyho1998 <44996214+garyho1998@users.noreply.github.com>
2021-04-17 19:59:52 +0800

    final

--- backend/dataprocessing/project_and_year_df.py
+++ backend/dataprocessing/project_and_year_df.py
@@ -13,18 +13,23 @@ def getFirstYear(years):
     return yearlist[0]
 
 
-project_df = pd.read_excel(current_file_path+"/clean_project.xlsx", sheet_name=0, engine='openpyxl')
-year_df = pd.read_excel(current_file_path+"/website_year_df.xlsx", sheet_name=0, engine='openpyxl').drop_duplicates("Reference Code")
+project_df = pd.read_excel(current_file_path+"/data_file/clean_project.xlsx", sheet_name=0, engine='openpyxl')
+year_df = pd.read_excel(current_file_path+"/data_file/website_year_df.xlsx", sheet_name=0, engine='openpyxl').drop_duplicates("Reference Code")
 year_df['year'] = year_df['year'].apply(getFirstYear)
 
 for index, row in project_df.iterrows():
     scores = ast.literal_eval(row['Score'])
     scores = [float(x) for x in scores]
     argmax = np.argmax(scores)
-    project_df.loc[index, "Sentence Output"] = ast.literal_eval(row['Sent'])[argmax]
-    project_df.loc[index, "Answer"] = ast.literal_eval(row['Answer'])[argmax]
+    sent = ast.literal_eval(row['Sent'])[argmax]
+    ans = ast.literal_eval(row['Answer'])[argmax]
+    if (len(sent)-len(ans) < 5):
+        sent = ""
+        ans = ""
+    project_df.loc[index, "Sentence Output"] = sent
+    project_df.loc[index, "Answer"] = ans
     
 
 result  = project_df.merge(year_df, left_on='Reference Code', right_on='Reference Code')
 result = result[['Reference Code', 'year', 'Sentence Output', 'Answer']]
-result.to_csv(current_file_path+"/project_and_year_one.csv")  
+result.to_csv(current_file_path+"/data_file/project_and_year_one.csv")  
--- backend/dataprocessing/test.py
+++
@@ -1,4 +0,0 @@
-import pathlib
-
-path = str(pathlib.Path(__file__).parent.absolute()) + '/1.list_it_solutions_modelEmbedding_model_stsb-roberta-large_numberOfWords_1000400_notLemmatize'
-print(path)
