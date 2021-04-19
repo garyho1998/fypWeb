4b1e46a1313a9b1ff8ce0cfd0c1ca5ba9827ca87
garyho1998 <44996214+garyho1998@users.noreply.github.com>
2021-04-19 15:08:08 +0800

    Revert "final"
    This reverts commit 945a64d44abd15a2e1d9f5efe4a13610eb17d551.

---
+++ backend/dataprocessing/test.py
@@ -0,0 +1,4 @@
+import pathlib
+
+path = str(pathlib.Path(__file__).parent.absolute()) + '/1.list_it_solutions_modelEmbedding_model_stsb-roberta-large_numberOfWords_1000400_notLemmatize'
+print(path)
